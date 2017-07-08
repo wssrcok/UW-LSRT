739047b364d1f6fd90785febe39dbea9bccc5a5f
wssrcok <wssrcok@uw.edu>
2017-07-08 00:03:18 -0700

    updated memory

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,5 +1,30 @@
 $(function() {
-	$('#button').click(function(){
-		$('#process').text('Now registering ' + $('#name').val());
+
+	chrome.storage.sync.get('registering', function(course) {
+		$('#process').text(course.registering);
 	})
-})
+
+	$('#button').click(function() {
+		chrome.storage.sync.get('registering', function(course) {
+			var newCourses = "";
+			if (course.registering != "") {
+				newCourses += course.registering;
+			}
+
+			var input = $('#name').val();
+
+			if (input == "clear") {
+				chrome.storage.sync.set({'registering': ""});
+				newCourses = "";
+			} else {
+				if (input != "") {
+					newCourses += "\nNow registering "  + input;
+				}
+
+				chrome.storage.sync.set({'registering': newCourses});
+			}
+			$('#process').text(newCourses);
+			$('#name').val('');
+		});
+	});
+});
